df528a33576e89545f78ee1ea6df3c69b0589de4
VINAY\vinay <vinay.surisetty@snowatech.com>
2024-10-15 21:21:23 +0530

    Updated campaign pages

--- src/elements/Campaigns/ManageCampaigns.js
+++ src/elements/Campaigns/ManageCampaigns.js
@@ -1,72 +1,124 @@
 import React, { useEffect, useState,useRef, useMemo } from 'react';
-import { Box, Button, MenuItem, Select, TextField, Typography , InputLabel,FormControl } from "@mui/material";
+import { Box, Button, MenuItem, Select, Typography } from "@mui/material";
 import { MantineReactTable, useMantineReactTable } from "mantine-react-table";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector} from "react-redux";
 import { getCampaigns } from '../../store/actions/campaign-action';
-import { FaLongArrowAltRight, FaLongArrowAltLeft } from "react-icons/fa";
+import { FaLongArrowAltRight, FaLongArrowAltLeft, FaEdit } from "react-icons/fa";
+import { MdOutlineDelete } from "react-icons/md";
 
 const ManageCampaigns = () => {
 
   const dispatch = useDispatch();
-  const fetchOnce = useRef(false);
+  const fetchOnce = useRef(false); // Reference to ensure campaigns are fetched only once
+  const isActionChange = useRef(false);
 
   const navigate = useNavigate();
 
-  const [filters, setFilters] = useState( { pageNumber : 1, pageCount : 5 });
-  const allCampaigns = useSelector(state => state.getCampaignsReducer.campaigns);
+  const [pagination, setPagination] = useState( { pageNumber : 1, pageCount : 5 }); // State to manage pagination filters (page number and count)
+  const allCampaigns = useSelector(state => state.getCampaignsReducer.campaigns); // Selector to get the campaigns from the Redux store
 
   useEffect(() => {
+    if(fetchOnce.current === true && isActionChange.current === true) {
+      dispatch(getCampaigns(pagination));
+    }
+  },[pagination]);
+
+  // useEffect to fetch campaigns only on the first render
+  useEffect(() => { 
     if (!fetchOnce.current) {
-      dispatch(getCampaigns(filters));
-      fetchOnce.current = true;  
+      dispatch(getCampaigns(pagination));
+      fetchOnce.current = true;   // Set the fetchOnce.current to true to prevent refetching
     }
   },[dispatch]); 
 
-  const handlePreviousPage = (e) => { //Handle previous page
-    setFilters({...filters, pageNumber : filters.pageNumber - 1});
-  }
+  const handlePreviousPage = (e) => {  // Function to handle the 'Previous Page' button click event
+    setPagination({...pagination, pageNumber : pagination.pageNumber - 1}); // Update the filters to go to the previous page
+    isActionChange.current = true;
+  };
 
-  const handleNextPage = (e) => { //Handle next page
-    setFilters({...filters, pageNumber : filters.pageNumber + 1});
-  }
+  const handleNextPage = (e) => {  // Function to handle the 'Next Page' button click event
+    setPagination({...pagination, pageNumber : pagination.pageNumber + 1}); // Update the filters to go to the next page
+    isActionChange.current = true;
+  };
 
-  const handlePageCount = (e) => { //Handle page count
-    console.log(filters.pageCount);
-    setFilters({...filters, pageCount : e.target.value, pageNumber : 1});
-  }
+  const handlePageCount = (e) => {  // Function to handle the page count selection (how many items to show per page)
+    setPagination({...pagination, pageCount : e.target.value, pageNumber : 1}); // Update the filters with the new page count and reset to page 1
+    isActionChange.current = true;
+  };
 
+  function formatToIST(dateString) {
+    // Create a new Date object from the input UTC date string
+    const date = new Date(dateString);
+    // Calculate the time in milliseconds for 5 hours and 30 minutes
+    const istOffset = 5.5 * 60 * 60 * 1000; // 5 hours 30 minutes in milliseconds
+    // Adjust the UTC time by adding the IST offset
+    const istDate = new Date(date.getTime() + istOffset);
+    // Options to format the date to the desired output
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    // Format the date to the desired format: October 10, 2024
+    return istDate.toLocaleDateString('en-US', options);
+  };
 
+  // Memoized columns configuration for the MantineReactTable
   const columns = useMemo( () => 
-  []);
+    [
+      { header:"Name", 
+        accessorFn: (row) => { return ( <Typography className = "data-column title">{row["c_nm"]}</Typography> )}, 
+      },
+      { header: "Duration", 
+        accessorFn: (row) => ( <Typography className='data-column large-width'> {`${formatToIST(row["start_dt"])} - ${formatToIST(row["end_dt"])}`}  </Typography> )
+      },
+      { header:"Created on", 
+        accessorFn: (row) => { return ( <Typography className='data-column date'>{formatToIST(row["c_on"])}</Typography> )}, 
+      },
+      {
+        header: "Status",
+        accessorFn: (row) => {
+          return row["status"] && row["status"] === 1 ? (
+            <Typography className='data-column active'>Active</Typography>
+          ) : (
+            <Typography className='data-column inactive'>Inactive</Typography>
+          );
+        },
+      },
+      { header:"Action", 
+        accessorFn: (row) => { return ( 
+        <Box className="action-column">
+          <Typography className='action-icon edit'><FaEdit/></Typography>
+          <Typography className='action-icon delete'><MdOutlineDelete/></Typography>
+        </Box> )}, 
+      },
+    ]
+  );
 
   const table = useMantineReactTable({
     columns, data: allCampaigns.campaigns || [],
-    renderTopToolbar: () => {
+    renderTopToolbar: () => { // Custom top toolbar for the table
       return (
-        <Box className="top-toolbar"> 
-          <Box className = "toolbar-heading">Campaigns </Box>
-          <Box className = "toolbar-heading">Total Campaigns count: 0 </Box>
+        <Box className="top-toolbar title"> 
+          <Box >Campaigns </Box>
+          <Box >Total Campaigns: {allCampaigns ? allCampaigns.totalCount : 0 } </Box>
         </Box>
       );
     },
-    renderBottomToolbar: () => {  
+    renderBottomToolbar: () => {  // Custom bottom toolbar for pagination controls
       return (
-        <Box className="bottom-toolbar-pagination">
+        <Box className="pagination-toolbar">
               <Box className="current-page">
                   <Typography>CurrentPage : </Typography>
-                  <Typography>{filters.pageNumber}</Typography>
+                  <Typography>{pagination.pageNumber}</Typography>
               </Box>
-              <Box className="sub-bottom-container">
-                  <Select className="pagesize-select" value={filters.pageCount} onChange={handlePageCount} >
+              <Box className="pagesize">
+                  <Select className="selected-page" value={pagination.pageCount} onChange={handlePageCount} >
                       <MenuItem value="5">5</MenuItem>
                       <MenuItem value="10">10</MenuItem>
                   </Select>
                   <Box>
-                      <Button onClick={handlePreviousPage} disabled = {filters.pageNumber === 1 }><FaLongArrowAltLeft /></Button> 
-                      {filters.pageNumber} 
-                      <Button onClick={handleNextPage} disabled = {(Math.ceil(allCampaigns.totalCount/filters.pageCount) === filters.pageNumber )
-                          || (allCampaigns.totalCount/filters.pageCount) === 0}> <FaLongArrowAltRight /></Button>
+                      <Button onClick={handlePreviousPage} disabled = {pagination.pageNumber === 1 }><FaLongArrowAltLeft /></Button> 
+                      {pagination.pageNumber} 
+                      <Button onClick={handleNextPage} disabled = {(Math.ceil(allCampaigns.totalCount/pagination.pageCount) === pagination.pageNumber )
+                          || (allCampaigns.totalCount/pagination.pageCount) === 0}> <FaLongArrowAltRight /></Button>
                   </Box>
               </Box>
           </Box>
@@ -77,9 +129,9 @@ const ManageCampaigns = () => {
   });
 
 return (
-  <Box className="table-container grid-main">
+  <Box className="manage-campaign">
     <Box className="page-title">Manage Campaigns</Box>
-    <Box className="search-box">
+    <Box className="primary-action">
         <Button className="button" onClick={() => navigate('/newcampaign')}> + New Campaign</Button>
     </Box>
     <Box className = "table-mantine">
--- src/elements/Campaigns/NewCampaign.js
+++ src/elements/Campaigns/NewCampaign.js
@@ -11,14 +11,21 @@ const Campaign = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [campaign, setCampaign] = useState({});
+  // State to store newCampaign form data
+  const [newCampaign, setNewCampaign] = useState({});
 
+  // State to handle validation errors
   const [validationErrors, setValidationErrors] = useState({});
+
+  // States to manage alert messages
   const [alertMessage, setAlertMessage] = useState('');
   const [alertSeverity, setAlertSeverity] = useState('info');
   const [showAlert, setShowAlert] = useState(false);
+
+  // State to track if the form was submitted
   const [submited, setSubmited] = useState(false);
 
+  // Selector to fetch the campaign addition response and error status from Redux store
   const addCampaign = useSelector(state => state.addNewCampaignReducer.newCampaign)
   const errorStatus = useSelector(state => state.addNewCampaignReducer.generalError);
 
@@ -26,36 +33,38 @@ const Campaign = () => {
     if (submited && errorStatus !== "") {
        showAlertMessage(errorStatus, 'error');
     } else if (submited && Object.keys(addCampaign).length > 0) {
-       showAlertMessage(addCampaign.message, 'success');
-       setTimeout(() => {
-           dispatch(resetNewCampaign());
-           setSubmited(false);
-           resetForm(); 
-         }, 2000); 
-   }
+      showAlertMessage(addCampaign.message, 'success');
+      setTimeout(() => { // Reset form and state after successful submission
+          dispatch(resetNewCampaign());
+          setSubmited(false);
+          resetForm(); 
+        }, 2000); 
+    }
   }, [addCampaign, errorStatus]);
 
   const handleChange = (event) => {
     const { name, value} = event.target;
-    setCampaign((prevState) => ({ ...prevState, [name]: value }));
+    setNewCampaign((prevState) => ({ ...prevState, [name]: value }));
 
-    setValidationErrors((prevErrors) => {
+    setValidationErrors((prevErrors) => { // Remove any existing validation error for the field
       const newErrors = { ...prevErrors };
       delete newErrors[name];
       return newErrors;
     });
   };
-
+ // Handle changes in date fields
   const handleDateChange = (name, newValue) => {
-    setCampaign((prevState) => ({ ...prevState, [name]: newValue }));
-    setValidationErrors((prevErrors) => {
+    setNewCampaign((prevState) => ({ ...prevState, [name]: newValue }));
+
+    setValidationErrors((prevErrors) => { // Remove any existing validation error for the field
         const newErrors = { ...prevErrors };
         delete newErrors[name];
         return newErrors;
       });
   };
-  const resetForm = () => {
-    setCampaign((prevState) => 
+  // Reset the form by clearing all input fields and errors
+  const resetForm = () => { 
+    setNewCampaign((prevState) => 
     Object.keys(prevState).reduce((acc,key) => {
       acc[key] = ""
       return acc;
@@ -65,6 +74,7 @@ const Campaign = () => {
     setAlertMessage('');
     setShowAlert(false);
   };
+  // Display alert message with a specified severity level
   const showAlertMessage = (message, severity = 'info', timeout = 5000) => {
     setAlertMessage(message);
     setAlertSeverity(severity);
@@ -73,78 +83,81 @@ const Campaign = () => {
       setShowAlert(false);
     }, timeout)
   };
+  // Hide alert message manually
   const hideAlert = () => {
     setShowAlert(false);
   };
 
+  // Validate the form by checking if required fields are filled
   const validateForm = () => {
     const errors = {};
     const requiredFields = ['c_nm', 'start_dt', 'end_dt']; // Fields that are required
 
     requiredFields.forEach((field) => {
-      if (!campaign[field]) {
+      if (!newCampaign[field]) {
         errors[field] = true;
       }
     });
-
     setValidationErrors(errors);
-    return Object.keys(errors).length === 0;
+    return Object.keys(errors).length === 0; // Return true if no errors
   };
 
+  // Handle form submission
   const handleSubmit = () => {
     if(validateForm()) {
-        dispatch(addNewCampaign(campaign));
+        dispatch(addNewCampaign(newCampaign)); // Dispatch the action to add the new campaign if validation passes
         setSubmited(true);
-
     }else {
-        showAlertMessage("Please fill all required fields", "error");
+        showAlertMessage("Please fill all required fields", "error"); // Show error alert if validation fails
     }
-  }
+  };
 
   return (
-    <Box className="campaign-main">
-      <Typography className="title">New Campaign</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <TextField  type="text"  label="Campaign Name" name="c_nm" value={campaign["c_nm"] || ""}   onChange={handleChange}  className={ validationErrors["c_nm"] ? "error-Validation" : "text-box"}  fullWidth required />
+    <Box className="new-campaign">
+      <Box className="header-container">
+        <Typography className="page-title">New Campaign</Typography>
+        {showAlert && ( <Stack className='alert'>  <Alert severity={alertSeverity} onClose={hideAlert}>  {alertMessage} </Alert>  </Stack> )}
+      </Box>
+      <Box className = "form-container">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item xs={4}>
+                  <TextField  type="text"  label="Campaign Name" name="c_nm" value={newCampaign["c_nm"] || ""}   onChange={handleChange}  className={ validationErrors["c_nm"] ? "error-Validation" : "text-box"}  fullWidth required />
+                </Grid>
               </Grid>
-            </Grid>
+          </Grid>
+          <Grid item xs={4}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker 
+                label="Start Date"  value={newCampaign["start_dt"] ? dayjs(newCampaign["start_dt"], "DD/MM/YYYY") : null}  required
+                onChange={(newValue) => handleDateChange('start_dt', newValue)}  minDate = {dayjs()}
+                renderInput={(params) => <TextField {...params} fullWidth  className={ validationErrors["start_dt"] ? "error-Validation" : "text-box"} />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={4}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker 
+                label="End Date"  value={newCampaign["end_dt"] ? dayjs(newCampaign["end_dt"], "DD/MM/YYYY") : null} required 
+                onChange={(newValue) => {
+                  const endDate = newValue ? dayjs(newValue).endOf('day') : null;
+                  handleDateChange('end_dt', endDate); 
+                }} minDate = {dayjs()}
+                renderInput={(params) => <TextField {...params} fullWidth className={ validationErrors["end_dt"] ? "error-Validation" : "text-box"} />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={8}>
+              <TextField  type="text"  label="Description" name="desc" value={newCampaign["desc"] || ""} onChange={handleChange}  className={ validationErrors["desc"] ? "error-Validation" : "text-box"}  fullWidth />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker 
-              label="Start Date"  value={campaign["start_dt"] ? dayjs(campaign["start_dt"], "DD/MM/YYYY") : null}  required
-              onChange={(newValue) => handleDateChange('start_dt', newValue)}  minDate = {dayjs()}
-              renderInput={(params) => <TextField {...params} fullWidth  sx={{ width: "385px" }} className={ validationErrors["start_dt"] ? "error-Validation" : "text-box"} />}
-            />
-          </LocalizationProvider>
-        </Grid>
-        <Grid item xs={4}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker 
-              label="End Date"  value={campaign["end_dt"] ? dayjs(campaign["end_dt"], "DD/MM/YYYY") : null} required 
-              onChange={(newValue) => {
-                const endDate = newValue ? dayjs(newValue).endOf('day') : null;
-                handleDateChange('end_dt', endDate); // Fixed key from 'start_dt' to 'end_dt'
-              }} minDate = {dayjs()}
-              renderInput={(params) => <TextField {...params} fullWidth className={ validationErrors["end_dt"] ? "error-Validation" : "text-box"} />}
-            />
-          </LocalizationProvider>
-        </Grid>
-        <Grid item xs={8}>
-            <TextField  type="text"  label="Description" name="desc" value={campaign["desc"] || ""} onChange={handleChange}  className={ validationErrors["desc"] ? "error-Validation" : "text-box"}  fullWidth />
-            </Grid>
-      </Grid>
-      <Box className="buttons-container">
-            <Box className="all-buttons">
-               <Button type='button' className='cancel-button' onClick={() => navigate('/campaigns')} >Cancel</Button>
-                <Button type='button' className='cancel-button' onClick={resetForm} >Reset</Button>
-                <Button type='submit' className='add-button' onClick={handleSubmit}>Save</Button>
-            </Box>
-            {showAlert && ( <Stack className='alert'>  <Alert severity={alertSeverity} onClose={hideAlert}>  {alertMessage} </Alert>  </Stack> )}
-     </Box>
+        <Box className="actions-buttons">
+              <Button type='button' className='cancel' onClick={() => navigate('/campaigns')} >Cancel</Button>
+              <Button type='button' className='cancel' onClick={resetForm} >Reset</Button>
+              <Button type='submit' className='add' onClick={handleSubmit}>Save</Button>
+        </Box>
+      </Box>
     </Box>
   );
 };
